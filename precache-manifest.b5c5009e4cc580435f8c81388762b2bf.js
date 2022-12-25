d0f16a12d11bb7dfc281123b534cdf74a8e805c4
pardo30 <info@pardo30.com>
2022-12-25 12:37:06 +0100

    Updates

---
+++ precache-manifest.b5c5009e4cc580435f8c81388762b2bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f634cefbb4b8b3b12a68",
+    "url": "/jpa2209/static/css/main.1f1349e2.chunk.css"
+  },
+  {
+    "revision": "f634cefbb4b8b3b12a68",
+    "url": "/jpa2209/static/js/main.f634cefb.chunk.js"
+  },
+  {
+    "revision": "fe219de1fcb0aa2503d7",
+    "url": "/jpa2209/static/js/1.fe219de1.chunk.js"
+  },
+  {
+    "revision": "5e14edcd0ad74c039a1f",
+    "url": "/jpa2209/static/js/runtime~main.5e14edcd.js"
+  },
+  {
+    "revision": "bfeb8ea233c537cd5c817b97d57b3d5f",
+    "url": "/jpa2209/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jpa2209/precache-manifest.7b9562b8a0649f28d470a2d840adc0c2.js"
+  "/jpa2209/precache-manifest.b5c5009e4cc580435f8c81388762b2bf.js"
 );
 
 workbox.clientsClaim();
