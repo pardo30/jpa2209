dff4dda657d2721b54bacc4fe144e39b5c296d6e
pardo30 <info@pardo30.com>
2022-12-25 12:21:02 +0100

    Updates

---
+++ precache-manifest.519e741e02a03e9b4d7a66a44b548953.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97bad12b0763b6b438f7",
+    "url": "/jpa2209/static/css/main.1f1349e2.chunk.css"
+  },
+  {
+    "revision": "97bad12b0763b6b438f7",
+    "url": "/jpa2209/static/js/main.97bad12b.chunk.js"
+  },
+  {
+    "revision": "fe219de1fcb0aa2503d7",
+    "url": "/jpa2209/static/js/1.fe219de1.chunk.js"
+  },
+  {
+    "revision": "5e14edcd0ad74c039a1f",
+    "url": "/jpa2209/static/js/runtime~main.5e14edcd.js"
+  },
+  {
+    "revision": "ba76fc4d972fe4321f707332993c1171",
+    "url": "/jpa2209/index.html"
+  }
+];
